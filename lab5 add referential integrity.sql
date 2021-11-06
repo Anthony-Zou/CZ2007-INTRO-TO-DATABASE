d37d3160c4d7e52b4c3f8f3aa63f6c96a8e53c80
Xunyi <73570047+XunyiiZ@users.noreply.github.com>
2021-11-06 21:32:03 +0800

    Add files via upload

---
+++ lab5 add referential integrity.sql
@@ -0,0 +1,146 @@
+CREATE TABLE [CUSTOMER] (  
+  [Id] INT NOT NULL 
+  [Phone_num] VARCHAR(8) NOT NULL, 
+  [Username] VARCHAR(30) UNIQUE NOT NULL, 
+  [Email] VARCHAR(50) UNIQUE NOT NULL, 
+  [Password] VARCHAR(50) NOT NULL,
+  [Full_name] VARCHAR(30) NOT NULL, 
+  [Address] VARCHAR(50) NOT NULL,
+  PRIMARY KEY ([Id]), 
+); 
+ 
+CREATE TABLE [CREDIT_CARD] ( 
+  [Card_num] VARCHAR(16) NOT NULL, 
+  [Customer_id] INT NOT NULL,                               
+  [Bank] VARCHAR(20) NOT NULL, 
+  [Date_valid_to] DATE NOT NULL, 
+  [Date_valid_from] DATE NOT NULL, 
+  PRIMARY KEY ([Card_num]),  
+  FOREIGN KEY (Customer_id) REFERENCES CUSTOMER(Id) 
+  ON DELETE NO ACTION    
+  ON UPDATE NO ACTION
+); 
+ 
+CREATE TABLE [SHOP] ( 
+  [Id] INT NOT NULL, 
+  [Name] VARCHAR(50) NOT NULL,
+  PRIMARY KEY ([Id]),  
+); 
+ 
+ 
+CREATE TABLE [PRODUCT_TYPE] ( 
+  [Id] INT NOT NULL, 
+  [Parent_id] INT NOT NULL,
+  [Description] VARCHAR(300) NOT NULL, 
+  PRIMARY KEY ([Id]), 
+  FOREIGN KEY (Parent_id) REFERENCES PRODUCT_TYPE(Id)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+); 
+ 
+CREATE TABLE [RESTRICTED_TO] ( 
+  [Shop_id] INT NOT NULL, 
+  [Product_type_id] INT NOT NULL, 
+  PRIMARY KEY ([Shop_id], [Product_type_id]), 
+  FOREIGN KEY (Shop_id) REFERENCES SHOP(Id) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION, 
+  FOREIGN KEY (Product_type_id) REFERENCES PRODUCT_TYPE(Id) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION 
+); 
+ 
+ 
+CREATE TABLE [PRODUCT] ( 
+  [Id] INT NOT NULL, 
+  [Shop_id] INT NOT NULL, 
+  [Product_type_id] INT NOT NULL,  
+  [Name] VARCHAR(50) NOT NULL, 
+  [Colour] VARCHAR(10) NOT NULL, 
+  [Size] VARCHAR(5) NOT NULL, 
+  [Price] FLOAT(7)NOT NULL, 
+  [Description] VARCHAR(300) NOT NULL, 
+  PRIMARY KEY ([Id]), 
+  FOREIGN KEY (Shop_id) REFERENCES SHOP(Id) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION, 
+  FOREIGN KEY (Product_type_id) REFERENCES PRODUCT_TYPE(Id) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION 
+); 
+ 
+CREATE TABLE [PHOTO] ( 
+  [Seq] INT NOT NULL, 
+  [Product_id] INT NOT NULL, 
+  [Url] VARCHAR(50) NOT NULL, 
+  PRIMARY KEY ([Seq], [Product_id]), 
+  FOREIGN KEY (Product_id) REFERENCES PRODUCT(Id) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION
+); 
+ 
+CREATE TABLE [SHIPMENT] ( 
+  [Id] INT NOT NULL, 
+  [Date] DATE NOT NULL, 
+  PRIMARY KEY ([Id]), 
+  [Tracking_num] VARCHAR(30) UNIQUE,
+); 
+ 
+CREATE TABLE [ORDERS] ( 
+  [Id] INT NOT NULL, 
+  [Customer_id] INT NOT NULL, 
+  [Date] DATE NOT NULL, 
+  [Status] VARCHAR(10) DEFAULT 'processing',
+  PRIMARY KEY ([Id]),   
+  FOREIGN KEY (Customer_id) REFERENCES CUSTOMER(Id) 
+  ON DELETE NO ACTION  
+  ON UPDATE NO ACTION 
+); 
+ 
+CREATE TABLE [INVOICE] ( 
+  [Number] VARCHAR(10) NOT NULL, 
+  [Order_id] INT NOT NULL, 
+  [Date] DATE NOT NULL,  
+  [Status] VARCHAR(10) DEFAULT 'issued',
+  PRIMARY KEY ([Number]), 
+
+  FOREIGN KEY (Order_id) REFERENCES ORDERS(Id) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION
+); 
+ 
+CREATE TABLE [PAYMENT] ( 
+  [Id] INT, 
+  [Invoice_number] VARCHAR(10) NOT NULL, 
+  [Credit_card_num] VARCHAR(16) NOT NULL, 
+  [Amount] FLOAT(10) NOT NULL, 
+  PRIMARY KEY ([Id]), 
+
+  FOREIGN KEY (Invoice_number) REFERENCES INVOICE(Number)  
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION, 
+  FOREIGN KEY (Credit_card_num) REFERENCES CREDIT_CARD(Card_num) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION 
+); 
+ 
+CREATE TABLE [ORDER_ITEM] ( 
+  [Sequence_num] INT, 
+  [Order_id] INT, 
+  [Product_id] INT NOT NULL, 
+  [Shipment_id] INT ,
+  [Quantity] INT NOT NULL, 
+  [Status] VARCHAR(10) DEFAULT 'processing', 
+  [Product_unit_price] FLOAT(7) not null, 
+  PRIMARY KEY ([Sequence_num], [Order_id]), 
+
+  FOREIGN KEY (Order_id) REFERENCES ORDERS(Id) 
+  ON DELETE NO ACTION   
+  ON DELETE NO ACTION, 
+  FOREIGN KEY (Product_id) REFERENCES PRODUCT(Id) 
+  ON DELETE NO ACTION  
+  ON UPDATE NO ACTION, 
+  FOREIGN KEY (Shipment_id) REFERENCES SHIPMENT(Id)  
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION 
+);
